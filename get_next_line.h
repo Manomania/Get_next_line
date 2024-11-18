1d53b7f31fe52d9d6b00c5ae80a236d562d8333c
Mano <maxime.martinez96@hotmail.fr>
2024-11-18 16:50:31 +0100

    Feat(header): Add prototypes with BUFFER_SIZE

--- get_next_line.h
+++ get_next_line.h
@@ -2,7 +2,20 @@
 # define GET_NEXT_LINE_H
 
 # include <stdio.h>
+# include <unistd.h>
 # include <fcntl.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE
+# endif
+
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dest,const char *src, size_t size);
+char	*extract_line(char	*bloc);
+char	*update_line(char	*bloc);
 
 #endif
