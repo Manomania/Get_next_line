64ee5988d695bd98d236dd43c9aea662a23a4496
Mano <maxime.martinez96@hotmail.fr>
2024-11-18 16:50:11 +0100

    Feat(Function): add ft_strchr, ft_strlcpy, ft_strjoin, ft_strlen

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,10 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: logname <logname@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: maximart <maximart@42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/17 11:22:36 by logname           #+#    #+#             */
-/*   Updated: 2024/11/17 11:22:39 by logname          ###   ########.fr       */
+/*   Created: 2024/11/17 11:22:36 by maximart          #+#    #+#             */
+/*   Updated: 2024/11/18 15:11:17 by maximart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dest,const char *src, size_t size)
+{
+	size_t	i;
+
+	if (!src || !dest)
+		return (0);
+	if (size == 0)
+		return (ft_strlen(src));
+	i = 0;
+	while (src[i] && (i < (size - 1)))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*join;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1 && !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	join = malloc(len_s1 + len_s2 + 1);
+	if (!join)
+		return (NULL);
+	if (s1)
+		ft_strlcpy(join, s1, len_s1 + 1);
+	if (s2)
+		ft_strlcpy(join + len_s1, s2, len_s2 + 1);
+	return (join);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
