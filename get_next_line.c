52385f8ea39cb354f2c2d9c4c09c15efe5ab6d60
Mano <maxime.martinez96@hotmail.fr>
2024-11-18 16:51:05 +0100

    Feat(function): Add extract,update & get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -3,31 +3,94 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: logname <logname@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: maximart <maximart@42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/17 11:22:23 by logname           #+#    #+#             */
-/*   Updated: 2024/11/17 11:24:50 by logname          ###   ########.fr       */
+/*   Created: 2024/11/17 11:22:23 by maximart          #+#    #+#             */
+/*   Updated: 2024/11/18 15:11:28 by maximart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*extract_line(char	*bloc)
+{
+	size_t	i;
+	char	*line;
+
+	if (!bloc || !*bloc)
+		return (NULL);
+	i = 0;
+	while (bloc[i] && bloc[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + (bloc[i] == '\n') + 1));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (bloc[i] && bloc[i] != '\n')
+	{
+		line[i] = bloc[i];
+		i++;
+	}
+	if (bloc[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*update_line(char	*bloc)
+{
+	size_t	i;
+	size_t	j;
+	char	*new;
+
+	if (!bloc || !*bloc)
+		return (NULL);
+	i = 0;
+	while (bloc[i] && bloc[i] != '\n')
+		i++;
+	if (!bloc[i])
+	{
+		free(bloc);
+		return (NULL);
+	}
+	new = malloc(ft_strlen(bloc) - i + 1);
+	if (!new)
+	{
+		free(bloc);
+		return (NULL);
+	}
+	i++;
+	j = 0;
+	while (bloc[i])
+		new[j++] = bloc[i++];
+	new[j] = '\0';
+	free(bloc);
+	return (new);
+}
+
 char	*get_next_line(int fd)
 {
-	char	*file;
-	char	buffer[256];
-	ssize_t	bytesread;
+	static char	*temp;
+	char	buffer[BUFFER_SIZE + 1];
+	ssize_t	b_read;
+	char	*line;
 
-	file = open("test.txt", O_RDONLY);
-	if (!file)
-		return (1);
-	while ((bytesread = read(file, buffer, sizeof(buffer) - 1)) > 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	b_read = read(fd, buffer, BUFFER_SIZE);
+	while (!ft_strchr(temp, '\n') && (b_read > 0))
 	{
-		buffer(bytesread) = '\0';
-		printf("%s", buffer);
+		buffer[b_read] = '\0';
+		temp = ft_strjoin(temp, buffer);
+		if (!temp)
+			return (NULL);
 	}
-	if (bytesread > 0)
-		return (1);
-	close(file);
-	return (0);
+	if (b_read == -1)
+	{
+		free(temp);
+		return (NULL);
+	}
+	line = extract_line(temp);
+	temp = update_line(temp);
+	return (line);
 }
